7eed5e099033d300f27e08cd8f6a33e6571f8151
Alan Nichol <alan@golastmile.com>
2018-08-08 14:43:51 +0200

    remove index from root path

--- sphinxcontrib/versioning/versions.py
+++ sphinxcontrib/versioning/versions.py
@@ -229,13 +229,15 @@ class Versions(object):
         :rtype: str
         """
         is_root = self.context['scv_is_root']
-        pagename = self.context['pagename']
+        pagename = self.context['pagename'].replace('index','')
         if self.context['current_version'] == other_version and not is_root:
+            #_page = pagename.split('/')[-1]
+            #_page = '' if _page == 'index' else _page
             return '{}/'.format(pagename.split('/')[-1])
 
         other_remote = self[other_version]
         other_root_dir = other_remote['root_dir']
         components = ['..'] * (pagename.count('/') + 1 )
         components += [other_root_dir] if is_root else ['..', other_root_dir]
-        components += [pagename if self.vhasdoc(other_version) else other_remote['master_doc']]
+        components += [pagename if self.vhasdoc(other_version) else other_remote['master_doc'].replace('index','')]
         return '{}/'.format(__import__('posixpath').join(*components))
